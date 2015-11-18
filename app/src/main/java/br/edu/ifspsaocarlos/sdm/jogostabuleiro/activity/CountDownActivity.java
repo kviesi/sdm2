767ba0ee35b21c5e25a7b5c7027977493fe38781
gustavo <guztawu@hotmail.com>
2015-11-18 19:54:48 -0200

    Implementacao dos contadores

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/jogostabuleiro/activity/CountDownActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/jogostabuleiro/activity/CountDownActivity.java
@@ -2,15 +2,14 @@ package br.edu.ifspsaocarlos.sdm.jogostabuleiro.activity;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
 import br.edu.ifspsaocarlos.sdm.jogostabuleiro.R;
+import br.edu.ifspsaocarlos.sdm.jogostabuleiro.domain.CountDown;
 
 public class CountDownActivity extends AppCompatActivity {
 
@@ -20,10 +19,10 @@ public class CountDownActivity extends AppCompatActivity {
     private TextView timerPlayer1;
     private TextView timerPlayer2;
 
-    private final long startTime = 30 * 1000;
-    private final String backgroudColor = "#1c484a";
-
     private boolean isPlayer1Active = Boolean.FALSE;
+
+    public static final String BACKGROUND_COLOR = "#1c484a";
+    public static final String LOSE_BACKGROUND_COLOR = "#FFC4AEB4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +32,8 @@ public class CountDownActivity extends AppCompatActivity {
         timerPlayer1 = (TextView) findViewById(R.id.timerPlayer1);
         timerPlayer2 = (TextView) findViewById(R.id.timerPlayer2);
 
-        countDownPlayer1 = new CountDown(startTime, timerPlayer1);
-        countDownPlayer2 = new CountDown(startTime, timerPlayer2);
+        countDownPlayer1 = CountDown.newInstance(CountDown.START_TIME, timerPlayer1);
+        countDownPlayer2 = CountDown.newInstance(CountDown.START_TIME, timerPlayer2);
     }
 
     @Override
@@ -53,18 +52,10 @@ public class CountDownActivity extends AppCompatActivity {
         Long startP2 = savedInstanceState.getLong("startP2");
         isPlayer1Active = savedInstanceState.getBoolean("isPlayer1Active");
 
-        if (startP1 == null || startP1 == 0) {
-            countDownPlayer1 = new CountDown(startTime, timerPlayer1);
-        } else {
-            countDownPlayer1 = new CountDown(startP1, timerPlayer1);
-        }
+        countDownPlayer1 = CountDown.newInstance(startP1, timerPlayer1);
+        countDownPlayer2 = CountDown.newInstance(startP2, timerPlayer2);
 
-        if (startP2 == null || startP2 == 0) {
-            countDownPlayer2 = new CountDown(startTime, timerPlayer2);
-        } else {
-            countDownPlayer2 = new CountDown(startP2, timerPlayer2);
-        }
-
+        //Verifica qual player estava ativo para reiniciá-lo
         if (isPlayer1Active) {
             countDownPlayer1.start();
         } else {
@@ -76,71 +67,29 @@ public class CountDownActivity extends AppCompatActivity {
 
     public void onClickPlayer1(View view) {
         isPlayer1Active = Boolean.FALSE;
-        timerPlayer1.setText(toHourMinuteAndSecond(startTime));
+        timerPlayer1.setText(toHourMinuteAndSecond(CountDown.START_TIME));
         timerPlayer1.setClickable(false);
         countDownPlayer1.cancel();
         timerPlayer2.setClickable(true);
-        timerPlayer2.setBackgroundColor(Color.parseColor(backgroudColor));
+        timerPlayer2.setBackgroundColor(Color.parseColor(BACKGROUND_COLOR));
         countDownPlayer2.start();
     }
 
     public void onClickPlayer2(View view) {
         isPlayer1Active = Boolean.TRUE;
-        timerPlayer2.setText(toHourMinuteAndSecond(startTime));
+        timerPlayer2.setText(toHourMinuteAndSecond(CountDown.START_TIME));
         timerPlayer2.setClickable(false);
         countDownPlayer2.cancel();
         timerPlayer1.setClickable(true);
-        timerPlayer1.setBackgroundColor(Color.parseColor(backgroudColor));
+        timerPlayer1.setBackgroundColor(Color.parseColor(BACKGROUND_COLOR));
         countDownPlayer1.start();
     }
 
-    private static String toHourMinuteAndSecond(long millis) {
+    public static String toHourMinuteAndSecond(long millis) {
         String hms = String.format("%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(millis),
                 TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
                 TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
         return hms;
     }
 
-
-    public class CountDown extends CountDownTimer implements Serializable {
-
-        private static final long serialVersionUID = 7455734982142331027L;
-
-        private TextView timerPlayer;
-        private Color lastColor;
-
-        private long timeUntilFinish;
-
-        public CountDown(long startTime, TextView timerPlayer) {
-            super(startTime, 1 * 1000);
-            this.timerPlayer = timerPlayer;
-        }
-
-        @Override
-        public void onFinish() {
-            timerPlayer.setBackgroundColor(Color.BLUE);
-            this.timerPlayer.setText("PERDEU!");
-        }
-
-        @Override
-        public void onTick(long millis) {
-            if (millis < 11 * 1000) {
-                if ((millis / 1000) % 2 == 0) {
-                    timerPlayer.setBackgroundColor(Color.RED);
-                } else {
-                    timerPlayer.setBackgroundColor(Color.parseColor(backgroudColor));
-                }
-            }
-            this.timerPlayer.setText(toHourMinuteAndSecond(millis));
-            timeUntilFinish = millis;
-        }
-
-        public void setTimerPlayer(TextView timerPlayer) {
-            this.timerPlayer = timerPlayer;
-        }
-
-        public long getTimeUntilFinish() {
-            return timeUntilFinish;
-        }
-    }
 }
